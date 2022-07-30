bb827541c8c450dee7dc71498cb96f4f6dcb8740
Muhammad Ammar <82372110+MuhammadAmmar26627@users.noreply.github.com>
2022-07-30 21:40:17 +0500

    Update streamlit_tetorial.py

--- App/streamlit_tetorial.py
+++ App/streamlit_tetorial.py
@@ -80,7 +80,7 @@ def load_saved_artifacts():
     __pdDistric=__data_columns[-9:]
     __data_columns=np.array(__data_columns)
 #     model=open(r'bag_model56.pkl','rb')
-    __model = pickle.load(open(r'bag_model56.pkl, 'rb'))
+    __model = pickle.load(open(r'bag_model56.pkl', 'rb'))
 #     __model=joblib.load(model)
 
 
